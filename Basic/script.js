e9caeeff63c36bf0bd32b5aa793028c68109f68c
Panagiotis Kyriazopoulos <panos_kyria@hotmail.com>
2015-04-24 17:17:04 +0300

    Finished the basic toDoList

---
+++ Basic/script.js
@@ -0,0 +1,18 @@
+var theList = document.getElementById('the-list');
+document.getElementById('save').addEventListener('click', function(e) {
+  e.preventDefault();
+  localStorage.setItem('toDoList', theList.innerHTML);
+});
+document.getElementById('clear').addEventListener('click', function(e) {
+  e.preventDefault();
+  localStorage.clear();
+  location.reload();
+});
+
+loadList();
+
+function loadList() {
+  if ( localStorage.getItem('toDoList') ) {
+    theList.innerHTML = localStorage.getItem('toDoList');
+  }
+}
